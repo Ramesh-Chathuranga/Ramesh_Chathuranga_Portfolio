8c8f41fe0c74dd8ae7c7f59f8e04e4d3fdeae3e9
Ramesh Chathuranga <rmrcchathuranga@gmail.com>
2025-03-17 11:36:13 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
-  // base: "/Ramesh_Chathuranga_Portfolio", 
+  base: "/Ramesh_Chathuranga_Portfolio", 
 })
